4fa8588ec26772fcd15c391ed1ef5e415ba461d4
Lindexy <novus7827@gmail.com>
2021-12-02 18:18:42 +0200

    minor update client

--- client/frontend.js
+++ client/frontend.js
@@ -1,25 +1,39 @@
-import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.esm.browser.js'
-
-async function getCards() {
-    let myFistCard = await request('/api/cards');
-    console.log(myFistCard);
-}
-getCards()
-
-
-var app4 = new Vue({
-    el: '#app-4',
+const App = {
     data() {
         return {
             cards: []
         }
     },
     async mounted() {
-        this.loading = true
-        this.cards = await request('/api/cards')
-        this.loading = false
-      }
-})
+        this.cards = await request('api/cards')
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Vue.createApp(App).mount('#app')
 
 
 async function request(url, method = 'GET', data = null) {
